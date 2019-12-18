b418c19de32eba7b69e03e1b1e23aaed12cbc359
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-18 18:05:46 +0000

    Updated React components to include CSS class names.

--- js/Grid.tsx
+++ js/Grid.tsx
@@ -1,8 +1,11 @@
-let Row = () => {
-    return <div class="">
+let Row = (props) => {
+    return <div className="row">
+        {props.content}
     </div>
 };
 
-let Column = () => {
-
+let Column = (props) => {
+    return <div className={"col-" + props.width}>
+        {props.content}
+    </div>
 };
